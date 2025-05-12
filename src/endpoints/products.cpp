04fcbe273ba5a1fd25170a57d1a3a79357df5873
NickTacke <the.nick.tacke@gmail.com>
2025-05-12 20:29:25 +0100

    Add implementation of getPage method in ProductsEndpoint class for retrieving product data from Lightspeed API.

---
+++ src/endpoints/products.cpp
@@ -0,0 +1,15 @@
+#include "products.h"
+#include "lightspeed.h"
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+ProductsEndpoint::ProductsEndpoint(LightspeedApi &api) : api_(api) {}
+
+std::string ProductsEndpoint::getPage(int page) {
+  std::vector<std::pair<std::string, std::string>> queryParams = {
+      {"page", std::to_string(page)}, {"limit", "250"}};
+
+  std::string pageJson = api_.performGetRequest("products.json", queryParams);
+  return pageJson;
+}
