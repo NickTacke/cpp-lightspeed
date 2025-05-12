19f3a73855365b3d0815ff3d83705bc8ea090695
NickTacke <the.nick.tacke@gmail.com>
2025-05-12 19:09:32 +0100

    Add initial implementation of LightspeedApi class for API interaction with configurable endpoints

---
+++ include/lightspeed.h
@@ -0,0 +1,28 @@
+#ifndef LIGHTSPEED_H
+#define LIGHTSPEED_H
+
+#include <string>
+#include <vector>
+
+// Endpoint includes
+
+class LightspeedApi {
+public:
+  enum class Cluster { EU1, US1 };
+
+  LightspeedApi(const std::string &apiKey, const std::string &apiSecret,
+                Cluster cluster, const std::string &language = "en");
+  LightspeedApi(const std::string &apiKey, const std::string &apiSecret,
+                const std::string &customBaseUrl);
+
+private:
+  std::string apiKey_;
+  std::string apiSecret_;
+  std::string baseUrlHost_;
+  std::string baseUrlPath_;
+
+  std::string getFullPath(const std::string &endpoint) const;
+  std::string performRequest(); // TODO: Implement
+};
+
+#endif
