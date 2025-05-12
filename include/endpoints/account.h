c7f3804ca784be852f2ac62096898de4c5dd2cf6
NickTacke <the.nick.tacke@gmail.com>
2025-05-12 23:42:14 +0200

    Add AccountEndpoint class for managing account-related API interactions, including retrieval of account data.

---
+++ include/endpoints/account.h
@@ -0,0 +1,23 @@
+#ifndef LIGHTSPEED_ENDPOINTS_ACCOUNT_H
+#define LIGHTSPEED_ENDPOINTS_ACCOUNT_H
+
+#include "../lightspeed.h"
+#include "../dtos/Account.h"
+
+class LightspeedApi; // Forward declaration
+
+namespace Lightspeed {
+namespace endpoints {
+
+class AccountEndpoint {
+public:
+  AccountEndpoint(LightspeedApi &api);
+  Lightspeed::dto::Account get();
+private:
+  LightspeedApi &api_;
+};
+
+} // namespace endpoints
+} // namespace Lightspeed
+
+#endif // LIGHTSPEED_ENDPOINTS_ACCOUNT_H
