49deaa96952a6781104e23a12ca4748151663af8
NickTacke <the.nick.tacke@gmail.com>
2025-05-12 23:49:03 +0200

    Add AccountEndpoint implementation for account data retrieval, utilizing JSON parsing for API interactions.

---
+++ src/endpoints/account.cpp
@@ -0,0 +1,21 @@
+// Relative paths for library files
+#include "../../include/endpoints/account.h"
+#include "../../include/dtos/Account.h"
+#include "../../include/lightspeed.h"
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace Lightspeed {
+namespace endpoints {
+
+AccountEndpoint::AccountEndpoint(LightspeedApi &api) : api_(api) {}
+
+Lightspeed::dto::Account AccountEndpoint::get() {
+  std::string accountJson = api_.performGetRequest("account.json");
+  auto json = nlohmann::json::parse(accountJson);
+  return json.at("account").get<Lightspeed::dto::Account>();
+}
+
+} // namespace endpoints
+} // namespace Lightspeed
