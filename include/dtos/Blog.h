985433d9c5cb8f9819c0ef33701549252ebf8e52
NickTacke <the.nick.tacke@gmail.com>
2025-05-13 16:02:26 +0200

    Add Blog DTO with JSON serialization support for managing blog entries in Lightspeed.

---
+++ include/dtos/Blog.h
@@ -0,0 +1,40 @@
+#ifndef LIGHTSPEED_DTOS_BLOG_H
+#define LIGHTSPEED_DTOS_BLOG_H
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace Lightspeed {
+namespace dto {
+
+enum Commentable {
+    no,
+    after_approval,
+    without_approval
+};
+
+struct Blog {
+    int id;
+    std::string createdAt;
+    std::string updatedAt;
+    int sortOrder;
+    Commentable commentable;
+    std::string url;
+    std::string title;
+    // TODO: Add resources
+};
+
+inline void from_json(const nlohmann::json &j, Blog &b) {
+    j.at("id").get_to(b.id);
+    j.at("createdAt").get_to(b.createdAt);
+    j.at("updatedAt").get_to(b.updatedAt);
+    j.at("sortOrder").get_to(b.sortOrder);
+    j.at("commentable").get_to(b.commentable);
+    j.at("url").get_to(b.url);
+    j.at("title").get_to(b.title);
+}
+
+} // namespace dto
+} // namespace Lightspeed
+
+#endif
