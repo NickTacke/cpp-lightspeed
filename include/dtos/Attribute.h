eaaff36000c88e73e5c4ea9ddbddca348bc23b1e
NickTacke <the.nick.tacke@gmail.com>
2025-05-13 16:01:41 +0200

    Add Attribute DTO with JSON serialization support for managing attributes in Lightspeed.

---
+++ include/dtos/Attribute.h
@@ -0,0 +1,25 @@
+#ifndef LIGHTSPEED_DTOS_ATTRIBUTE_H
+#define LIGHTSPEED_DTOS_ATTRIBUTE_H
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace Lightspeed {
+namespace dto {
+
+struct Attribute {
+    int id;
+    std::string title;
+    std::string defaultValue;
+};
+
+inline void from_json(const nlohmann::json &j, Attribute &a) {
+    j.at("id").get_to(a.id);
+    j.at("title").get_to(a.title);
+    j.at("defaultValue").get_to(a.defaultValue);
+}
+
+} // namespace dto
+} // namespace Lightspeed
+
+#endif
