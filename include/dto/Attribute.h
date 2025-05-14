4b12b5dddfb7d7b22bc7c238418d1bbac6efd3ba
NickTacke <the.nick.tacke@gmail.com>
2025-05-14 10:21:39 +0200

    Add JSON serialization support for Attribute by implementing to_json function.

--- include/dto/Attribute.h
+++ include/dto/Attribute.h
@@ -19,6 +19,14 @@ inline void from_json(const nlohmann::json &j, Attribute &a) {
     j.at("defaultValue").get_to(a.defaultValue);
 }
 
+inline void to_json(nlohmann::json &j, const Attribute &a) {
+    j = nlohmann::json{
+        {"id", a.id},
+        {"title", a.title},
+        {"defaultValue", a.defaultValue}
+    };
+}
+
 } // namespace dto
 } // namespace Lightspeed
 
